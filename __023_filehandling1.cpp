9bd2bc4a238120678266b6cf630b98dbae1e6c91
chetan <chetanshewag213@gmail.com>
2023-05-10 16:47:43 +0530

    minor correction

--- __023_filehandling1.cpp
+++ __023_filehandling1.cpp
@@ -6,31 +6,9 @@
 using namespace std;
 int main()
 {
-    fstream fout;
-    fout.open("myfile.txt");
-    if (!fout)
-    {
-        cout << "Error while creating the file";
-    }
-    else
-    {
-         fout << "hello chetan";
-        cout << "File created successfully";
-        fout.close();
-    }
-    return 0;
+   {
+    ofstream { "Hello.txt" };
+}
+ 
      
 }
-
-//   fstream FileName;
-//     FileName.open("FileName", ios::out);
-//     if (!FileName){
-//         cout<<"Error while creating the file";
-//     }
-//     else{
-//         cout<<"File created successfully";
-//         FileName.close();
-//     }
-//     return 0;
-
-// C:\MinGW\bin\g++.exe  "D:\Learning C\learning-c++\__023_program.cpp"
